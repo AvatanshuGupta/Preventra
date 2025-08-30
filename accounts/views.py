1c745540a4bbd5de6a87f28eec5ff445849e4468
AvatanshuGupta <avatanshugupta@gmail.com>
2025-08-31 01:38:20 +0530

    auth done

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.db import IntegrityError
 
-# Create your views here.
+def signup(request):
+    if request.method=='POST':
+        uname=request.POST.get('uname')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        fname=request.POST.get('fname')
+        lname=request.POST.get('lname')
+        try:
+            user = User.objects.create_user(username=uname, email=email, password=password,
+                                            first_name=fname, last_name=lname)
+            user.save()
+            return redirect('/login')
+        except IntegrityError:
+            return HttpResponse("Username already exists. Please choose a different one.")
+    return render(request,'signup.html')
+
+def login_view(request):
+    if request.method=='POST':
+        uname=request.POST.get('uname')
+        password=request.POST.get('password')
+        user=authenticate(request,username=uname,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("/core/home")
+    return render(request,'login.html')
+        
+
+def signout(request):
+    logout(request)
+    return redirect('/login')
+
+
